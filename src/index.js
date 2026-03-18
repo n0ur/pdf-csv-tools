2ebd00b9b50aaf266eb0e6192653d4b010600966
Nour <1746181+n0ur@users.noreply.github.com>
2026-03-18 18:05:02 +0100

    Define a new command for operation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import { Command } from "commander";
 import { pdf2csv } from "./pdf2csv/index.js";
 import { csvClean } from "./csv-clean/index.js";
+import { csvAggregate } from "./csv-aggregate/index.js";
 
 const DEFAULT_CONCURRENCY = "5";
 
@@ -10,7 +11,7 @@ const program = new Command();
 program
   .command("pdf2csv")
   .description("Convert PDF files to CSV")
-  .argument("<files...>", "PDF files to pdf2csv")
+  .argument("<files...>", "PDF files to convert")
   .requiredOption("-o, --output <dir>", "Output directory")
   .option(
     "-c, --concurrency <n>",
@@ -34,5 +35,21 @@ program
     DEFAULT_CONCURRENCY,
   )
   .action(csvClean);
+
+program
+  .command("csv-aggregate")
+  .description("Aggregate CSV files based on definitions")
+  .argument("<files...>", "CSV files to aggregate")
+  .requiredOption("-o, --output <dir>", "Output directory")
+  .requiredOption(
+    "-d, --definition <file>",
+    "aggregate definition file, check the template: config/aggregate_def_template.js",
+  )
+  .option(
+    "-c, --concurrency <n>",
+    "# of processes to run concurrently",
+    DEFAULT_CONCURRENCY,
+  )
+  .action(csvAggregate);
 
 program.parse();
